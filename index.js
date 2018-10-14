45d7be8ebc6d7a3ac84844a224844e21731a8aa6
begimov <begimov@gmail.com>
2018-10-14 07:57:35 +0300

    parsing url and retrieving requested path

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 var http = require('http');
+var url = require('url');
 
 var server = http.createServer(function(req, res) {
+
+    var parsedUrl = url.parse(req.url, true);
+
+    var path = parsedUrl.pathname;
+    var trimmedPath = path.replace(/^\/+|\/+$/g, '')
+
     res.end('Added string\n');
+
+    console.log(`${trimmedPath} requested`);
 })
 
 server.listen(3000, function() {
