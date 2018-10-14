30c92f8aae048bf70534ad9e7c8034ed9eea4127
begimov <begimov@gmail.com>
2018-10-14 07:41:16 +0300

    starting basic http server

---
+++ index.js
@@ -0,0 +1,9 @@
+var http = require('http');
+
+var server = http.createServer(function(req, res) {
+    res.end('Added string\n');
+})
+
+server.listen(3000, function() {
+    console.log('Server is listening');
+})
