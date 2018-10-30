aa82a560c9a0766b7467599d496295cb542d3f02
begimov <begimov@gmail.com>
2018-10-30 16:46:47 +0300

    storing new user data

--- config.js
+++ config.js
@@ -3,13 +3,15 @@ var enviroments = {};
 enviroments.staging = {
     'httpPort' : 3000,
     'httpsPort' : 3001,
-    'envName' : 'staging'
+    'envName' : 'staging',
+    'hashingSecret' : 'secret'
 };
 
 enviroments.production = {
     'httpPort' : 5000,
     'httpsPort' : 5001,
-    'envName' : 'production'
+    'envName' : 'production',
+    'hashingSecret' : 'secret'
 };
 
 var currentEnv = typeof(process.env.NODE_ENV) == 'string' 
